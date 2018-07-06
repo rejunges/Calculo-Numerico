3aee9497a651527d3c43097e3151bdea94affb85
Renata <rzjunges@inf.ufpel.edu.br>
2018-07-06 13:57:13 -0300

    Lista -> Numpy array, e correção do plot

--- MMQ.py
+++ MMQ.py
@@ -19,6 +19,8 @@ Argumentos:
 """
 
 #python3 MMQ.py -l 5 -c 2 -f 1 -v 0.30 2.75 4.50 5.95 7.80 1.80 1.90 3.10 3.90 3.30
+#python3 MMQ.py -l 10 -c 2 -f 1 -v 1 2 3 4 5 6 7 8 9 10 1.3 3.5 4.2 5 7 8.8 10.1 12.5 13 15.6
+#python3 MMQ.py -l 4 -c 2 -f 1 -v 1 2 3 4 3 5 6 8
 
 import argparse
 import matplotlib.pyplot as plt
@@ -39,17 +41,12 @@ valores = list(map(float, args["v"]))
 #Implementação da reta
 
 if (funcao == "1"):
-	x = valores[:int(len(valores)/2)]
-	y = valores[int(len(valores)/2):]
+	x = np.array(valores[:int(len(valores)/2)])
+	y = np.array(valores[int(len(valores)/2):])
 
-	x_quad = []
-	y_quad = []
-	x_y = []
-	for valor_x, valor_y in zip(x,y):
-		x_quad.append(round(valor_x*valor_x, 2))
-		y_quad.append(round(valor_y*valor_y, 2))
-		x_y.append(round(valor_x*valor_y, 2))
-
+	x_quad = x*x
+	y_quad = y*y
+	x_y = x*y
 	sum_x = sum(x)
 	sum_y = sum(y)
 	sum_x_quad = sum(x_quad)
@@ -58,19 +55,13 @@ if (funcao == "1"):
 
 	a0 = round(abs((sum_x_y * sum_x - sum_y * sum_x_quad)/(n* sum_x_quad - ((sum_x)*(sum_x)))), 2) #Aplicacao formula para a0
 	a1 = round((n * sum_x_y - sum_x * sum_y)/ (n*sum_x_quad - ((sum_x)*(sum_x))), 2) #Aplicacao formula para a1
+#	print(a0, a1)
 
-	#print(a0, a1)
-	result = []
-	for valor_x in x:
-		result.append(round(a0 + a1*valor_x, 2))
+	result = a0 + a1*x
+#	print(result)
 
-	#print(result)
-	plt.plot( x, y, 'go') # green bolinha
-	plt.plot(result, 'k-', color='blue')  # linha azul com a reta com os erros mínimos
-
-	#plt.axis([ 0, 8, 0, 10])
+	plt.plot( x, y, 'go', x, result, "-b") # linha azul com bolinhas verdes
 	plt.title("Reta com o mínimo erro")
-
 	plt.grid(True)
 	plt.xlabel("x")
 	plt.ylabel("y")

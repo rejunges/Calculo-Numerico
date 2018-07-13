f0536760d6ee3fd15884fe71cd10901ad1a6f04e
Karine <karinepestana>
2018-07-13 11:30:56 -0300

    Iniciando..

--- Derivacao.py
+++ Derivacao.py
@@ -15,6 +15,8 @@ Argumentos:
 	-f(x): função 
 """
 
+#python3 Derivacao.py -x 0.5 -H 0.5 -f 2/x-4
+
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
@@ -24,7 +26,21 @@ from scipy.linalg import lu_factor, lu_solve
 # Leitura dos argumentos por linha de comando
 ap = argparse.ArgumentParser()
 ap.add_argument('-x', "--x", required=True, help="Entrada x")
-ap.add_argument('-h', "--h", required=True, help="Entrada h")
-ap.add_argument('-f', "--f", required=True, nargs='+', help="Função")
+ap.add_argument('-H', "--h", required=True, help="Entrada h")
+ap.add_argument('-f', "--f", required=True, help="Funcao")
+
+args = vars(ap.parse_args())
+x = args["x"]
+h = args["h"]
+f = args["f"]
+
+print("X: ", x)
+print("H: ", h)
+print("F: ", f)
+
+fx = []
+
+def progressiva(fx):
+	return 0
 
 
